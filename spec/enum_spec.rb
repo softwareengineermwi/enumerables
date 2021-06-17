72449d61491142477bc9ddc9fc25e4fdd9ebfd96
Mwila Kaunda <mbkcust@gmail.com>
2021-06-17 16:24:08 +0200

    Fix linter errors

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-# rubocop:disable Metrics/BlockLength
 require './enumerable'
 
 describe Enumerable do
@@ -71,4 +68,3 @@ describe Enumerable do
     end
   end
 end
-# rubocop:enable Metrics/BlockLength
